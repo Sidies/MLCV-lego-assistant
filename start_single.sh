62a611ab805d99dbc700aade1f33df505cb54a2e
Marco <ugmqu@student.kit.edu>
2023-07-19 15:10:00 +0200

    added startups for single object

---
+++ start_single.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd ./jetson-inference
+docker/run.sh --volume ~/AISS_CV_Lego:/AISS -r "python3 ../AISS/frontend/app.py --detection=../AISS/models/Single_Object/Iteration_N_4/lego_Iteration_4.onnx --ssl-key=./data/key.pem --ssl-cert=./data/cert.pem --labels=../AISS/models/Single_Object/Iteration_N_4/lego_Iteration_4_labels.txt --input-layer=input_0 --output-layer=scores,boxes"
+
+
